7ff355329a8596ebffa65cfddc4538715e315c91
Oni <oniabifo@gmail.com>
2024-08-07 21:13:19 -0400

    first commit

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "private-provider.com/test/"
-      version = "3.107.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
   }
 }
