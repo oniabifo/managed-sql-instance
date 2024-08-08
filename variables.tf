f550d68a7a205a3bd64e48893f292b22ffdac3f4
Oni <oniabifo@gmail.com>
2024-08-08 10:01:00 -0400

    first commit

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  default     = "clickclaims-rgp-test-001"
+  default     = "clickclaims-rgp-test-0001"
 }
 
 variable "location" {
